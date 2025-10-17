b093e8eeb1c8c84104cc43c1a11316907df24cce
DLM4RI <mario.andres.castro.9.4@gmail.com>
2025-10-17 00:57:38 -0500

    Primer Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let none_1 = undefined
+var none = NaN
+ 
+function name(params) {
+    
+}
+
+
+if ( none == none_1) {
+
+} else {
+    console.log("hola")
+}
+
+
+
+array.forEach(element => {
+    
+});
